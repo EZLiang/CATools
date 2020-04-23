c45b2baac7aaf4368ac56af5c785513531135c23
EZLiang <47573436+EZLiang@users.noreply.github.com>
2020-04-23 18:19:48 -0400

    add main

---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+import circuitgen
+
+
+processes = {
+  "circuitgen": circuitgen.main
+}
+
+
+def main():
+  process = sys.argv[1]
+  args = sys.argv[2:]
+  if not (process in processes):
+    print("Program {} not found!".format(process))
+    return -1
+  try:
+    print(processes[process](*args))
+  except TypeError:
+    print("Invalid arguments!")
+    return -1
+  return 0
+
+
+if __name__ == "__main__":
+  main()
